4ea40cdcd5eb4db32012f6d0f91f5f6802850b59
Oluwajuwon Ibitoye <144378453+oluwajuwon-ibitoye@users.noreply.github.com>
2025-05-05 14:20:08 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+  
+  function openNav() {
+    document.getElementById("myNav").style.display = "block";
+  }
+
+  function closeNav() {
+    document.getElementById("myNav").style.display = "none";
+  }
+  
